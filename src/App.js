f8af7c031643661fa5550a2b5ac15ccecc809f12
Ivan Okello <ivandamieno99@gmail.com>
2022-11-03 17:11:53 +0300

    Developing task tracker

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+
+function App() {
+    
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctors Appointment',
+      day: 'Feb 5th at 2.30pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Doctors Appointment',
+      day: 'Feb 5th at 2.30pm',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Doctors Appointment',
+      day: 'Feb 5th at 2.30pm',
+      reminder: true,
+    },
+    {
+      id: 4,
+      text: 'Doctors Appointment',
+      day: 'Feb 5th at 2.30pm',
+      reminder: true,
+    },
+  ]
+  );
+  
+//  Delete Task
+  const deleteTask = (id) => {
+    console.log('delete', id);
+    setTasks(tasks.filter((task)=>task.id !== id))
+}
+
+  
+  return (
+    <div className="container">
+      <Header />
+      
+     {tasks.length>0?( <Tasks tasks={tasks} onDelete={deleteTask} />):('No tasks to display')}
+    </div>
+  );
+}
+
+export default App;
